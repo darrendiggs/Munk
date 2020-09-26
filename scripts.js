cff3c44d813a46138348903bdc47b85e50f3e7c8
Darren Diggs <darrendiggs00@gmail.com>
2020-09-25 19:08:23 -0500

    supplies polish/mock like btns

--- scripts.js
+++ scripts.js
@@ -1,17 +1,32 @@
 
+const productImgs = document.querySelectorAll(".expandable");
 
-document.querySelectorAll(".suppliesImg").addEventListener( "mouseover",function() {
-    document.querySelectorAll(".suppliesImg").classList.remove("suppliesImg")
-        document.querySelectorAll(".suppliesImg").classList.add("suppliesImgLg")
-    });
-    document.querySelectorAll(".suppliesImgLg").addEventListener( "mouseleave",function() {
-        document.querySelectorAll(".suppliesImgLg").classList.remove("suppliesImgLg")
-            document.querySelectorAll(".suppliesImgLg").classList.add("suppliesImg")
+for (i = 0; i <= (productImgs.length -1); ++i) {
+    productImgs[i].addEventListener( "mouseover", function() {
+        this.classList.remove("suppliesImg")
+        this.classList.add("suppliesImgLg")
         });
+    };
 
-        // this is to fix the mobile bug where if a product is click it gets stuck lg
-        document.querySelectorAll(".suppliesImg").addEventListener( "click",function() {
-            document.querySelectorAll(".suppliesImg").classList.remove("suppliesImglg")
-                document.querySelectorAll(".suppliesImg").classList.add("suppliesImg")
-            });
+for (i = 0; i <= (productImgs.length -1); ++i) {
+    productImgs[i].addEventListener( "mouseleave", function() {
+        this.classList.remove("suppliesImgLg")
+        this.classList.add("suppliesImg")
+        });
+    };
+
+    //this keeps the imgs from expanding when clicked in mobile view
+for (i = 0; i <= (productImgs.length -1); ++i) {
+    productImgs[i].addEventListener( "click", function() {
+        this.classList.remove("suppliesImgLg")
+        this.classList.add("suppliesImg")
+        });
+    };
+    
+
+    
+    for(i = 0; i <= (document.querySelectorAll(".blkHeart").length -1); ++i){
+document.querySelectorAll(".blkHeart")[i].addEventListener("click", function() {
+    this.innerHTML = "❤️";
+})}
 
